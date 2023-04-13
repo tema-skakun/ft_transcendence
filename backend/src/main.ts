f1a7033555503e9bd6ed3da0d7aefe005d765ee2
linu <jahn.linus22@gmail.com>
2023-04-13 21:02:01 +0200

    added optional containerization of the backend for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import * as cookieParser from 'cookie-parser';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true});
   app.useGlobalPipes(new ValidationPipe({
