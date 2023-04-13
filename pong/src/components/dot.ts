a71508320c5ae91ed4502641c9e21f1cc71893d3
linu <jahn.linus22@gmail.com>
2023-04-13 17:19:31 +0200

    integrated artems frontend, still fixes to be made

--- pong/src/components/dot.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from "../interfaces/config"; 
-import { GameState } from "../hooks/useSocket";
-
-
-export function dot(CONFIG: Config, drawingContext: CanvasRenderingContext2D, gameState: GameState) {
-
-	drawingContext.fillStyle = CONFIG.DOT_COLOR;
-	drawingContext.fillRect(gameState.dotCoordinate.x, gameState.dotCoordinate.y, CONFIG.DOT_WIDTH, CONFIG.DOT_HEIGHT);
-}
