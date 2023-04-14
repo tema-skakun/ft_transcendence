d838f8ad1fb2b9cf4f3985b475983601f4b754f1
Юля Грезина <ulagrezina@MacBook-Pro-admin.local>
2023-04-14 22:21:59 +0200

    WIP: added chat page

--- pong/src/index.tsx
+++ pong/src/index.tsx
@@ -1,21 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {renderEntireTree} from "./render";
 import state from "./Redux/state";
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App state={state}/>
-        </BrowserRouter>
-    </React.StrictMode>
-);
+
+renderEntireTree(state);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
