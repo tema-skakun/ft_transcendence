56ccf37512d1abcf46f399cb252d1b9828740658
linu <jahn.linus22@gmail.com>
2023-04-26 21:10:40 +0200

    added archivments and pug memes

--- backend/src/entities/index.ts
+++ backend/src/entities/index.ts
@@ -1,7 +1,8 @@
 import { User } from "src/entities/user/user.entity";
+import { Archivements } from "./archivements/archivments.entity";
 import { MatchHistoryEntry } from "./matchHistoryEntry/matchHistoryEntry.entity";
 
-const entities = [User, MatchHistoryEntry];
+const entities = [User, MatchHistoryEntry, Archivements];
 
 export {User};
 export default entities;
